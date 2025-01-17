d50ccfbd9eae23f5b6389e1a2894bafa226a37f2
Huw Downey <huwdowney27@gmail.com>
2025-01-17 21:26:44 +0000

    Skill prototyping and facing
    A parent class for skills, derived flamethrower class and making the player face the mouse

---
+++ BequestLegacy/Assets/Scripts/Flamethrower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class Flamethrower : Skills
+    {
+
+        private bool isToggled = false;
+        private GameObject Flames;
+        [SerializeField] private float offset = 1.0f;
+        public Flamethrower()
+        {
+        }
+
+        public override void Cast(Vector2 currentPosition, Quaternion currentRotation)
+        {
+            isToggled = !isToggled;
+            if (isToggled)
+            {
+                Vector2 facingVector = currentRotation * Vector2.up;
+                currentPosition = currentPosition + (facingVector * offset);
+                Flames = Instantiate(skillPrefab, currentPosition, (currentRotation * Quaternion.Euler(0, 0, 180)));
+                Flames.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+            else
+            {
+                Destroy(Flames);
+            }
+        }
+
+    }
+}
---
+++ BequestLegacy/Assets/Scripts/Skills.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal abstract class Skills : MonoBehaviour
+    {
+        public Vector2 position;
+
+        public float damage;
+        public float attackSpeed;
+        public float lifeTime;
+
+        public GameObject skillPrefab;
+
+        public Skills()
+        {
+            position = Vector2.zero;
+            damage = 0f;
+            attackSpeed = 0f;
+            lifeTime = 0f;
+        }
+
+        public abstract void Cast(Vector2 currentPosition, Quaternion currentRotation);
+    }
+}
